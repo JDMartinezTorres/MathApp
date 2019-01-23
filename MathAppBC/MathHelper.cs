863a135540fbc87e1f9f63f9d841c61ae33faa72
Brandon Curtiss <curtissbd17@gmail.com>
2019-01-23 14:36:23 -0500

    Add project files.

---
+++ MathAppBC/MathHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAppBC
+{
+    public class MathHelper
+    {
+        double z;
+        bool c = true;
+        double x;
+        double y;
+        public void  askUserForNumbers()
+        {
+            Console.WriteLine("Please enter the first number.");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number.");
+            y = double.Parse(Console.ReadLine());
+            
+        }
+        public double CalculationHelper()
+        {
+
+                while (c == true)
+                {
+                    Console.WriteLine("Please type +, -, *, /, or % for mathematical assistance or type anything else to exit.");
+                    string starter = (Console.ReadLine());
+                    if (starter == "+")
+                    {
+                        askUserForNumbers();
+                         z=x + y;
+                         Console.WriteLine($"The answer is {z:n}");
+                       return CalculationHelper();
+                    }
+                    if (starter == "-")
+                    {
+                        askUserForNumbers();
+                        z= x - y;
+                        Console.WriteLine($"The answer is {z:n}");
+                          return CalculationHelper();
+                    }
+                    if (starter == "*")
+                    {
+                        askUserForNumbers();
+                        z= x * y;
+                         Console.WriteLine($"The answer is {z:n}");
+                         return CalculationHelper();
+                    }
+                    if (starter == "/")
+                    {
+                        askUserForNumbers();
+                        if (y == 0)
+                        {
+                            Console.WriteLine("Division by zero is invalid. Please enter in a valid number 2.");
+                            y = double.Parse(Console.ReadLine());
+                             z= x / y;
+                             Console.WriteLine($"The answer is {z:n}");
+                             return CalculationHelper();
+                        }
+                        else
+                        {
+                            z= x / y;
+                             Console.WriteLine($"The answer is {z:n}");
+                             return CalculationHelper();
+                        }
+                    }
+                    if (starter == "%")
+                    {
+                        askUserForNumbers();
+                        z= x % y;
+                         Console.WriteLine($"The answer is {z:n}");
+                         return CalculationHelper();
+                    }
+                    if (starter != "+" || starter != "-" || starter != "*" || starter != "/" || starter != "%" )
+                    {
+                    Console.WriteLine("Thanks for stopping by!");
+                    c = false;
+                    }
+                  
+            }
+            return z;
+        }
+    }
+}
---
+++ MathAppBC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAppBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mathHelper = new MathHelper();
+            mathHelper.CalculationHelper();
+            Console.ReadLine();
+        }
+    }
+}
